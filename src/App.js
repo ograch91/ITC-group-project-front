875beda010dae2152f8fa10c9673e030692e01ff
Amnon Carmi <ace7884@hotmail.com>
2022-12-04 15:03:59 +0200

    Navbar height adjust

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import * as React from 'react';
 import { Navbar } from './Components/Layout/Navbar/Navbar';
 import "./App.css";
 import { HomePage } from "./Components/Views/HomePage/HomePage"
-// import { ProfilePage } from './Components/Views/ProfilePage/ProfilePage';
 function App() {
   return (
     <div className="App">
   <Navbar/>
     <HomePage/>
-    {/* <ProfilePage/> */}
    </div>
     );
 }
