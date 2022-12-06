50516a66f9b0cc9198f2ba80323bfebc7d25328a
Amnon Carmi <ace7884@hotmail.com>
2022-12-06 08:27:18 +0200

    Message static implemented

--- src/Components/Views/HomePage/HomePage.jsx
+++ src/Components/Views/HomePage/HomePage.jsx
@@ -1,13 +1,14 @@
 import { Header } from "../../StaticElements/Header/Header";
 import { ContentWrapper } from "../../Layout/ContentWrapper/ContentWrapper";
 import styles from "./HomePage.module.css";
+import { Message } from "../../StaticElements/Message/Message";
 
 export const HomePage = () => {
 
   return (
     <div className={styles.HomePage}>
-    <Header title="Welcome Header"/>
-    <ContentWrapper></ContentWrapper>
+    <Message id={"id"} chatid={"chatid"} sender={"Mosh"} datesent={"23.2.22"} content={"blah blah blah blah..."}/>
+    <Message id={"id"} chatid={"chatid"} sender={"MoshJunior"} datesent={"23.2.22"} content={"Yada Yada Yada..."}/>
     </div>
   );
 };
