6aaf29d878c88f4864efe9dbbebf8c214d283fe8
Amnon Carmi <ace7884@hotmail.com>
2022-12-15 14:43:36 +0200

    searchfield debounce implemented

--- src/Components/ActiveElements/SearchField/SearchField.jsx
+++ src/Components/ActiveElements/SearchField/SearchField.jsx
@@ -1,8 +1,35 @@
-import { TextField } from "@mui/material"
-import styles from "../SearchField/SearchField.module.css"
+import { TextField } from "@mui/material";
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+import styles from "../SearchField/SearchField.module.css";
 
 export const SearchField = () => {
+
+  const usersList = [
+    { Name: "The Shawshank Redemption", id: 1234 },
+    { Name: "newman", id: 4321 },
+    { Name: "BizzaroJerry", id: 1138 },
+  ];
+
   return (
-    <TextField className={styles.SearchField}id="outlined-basic" label="Search for user" variant="outlined" />
-    )
-}
+    // <TextField className={styles.SearchField}id="outlined-basic" label="Search for user" variant="outlined" />
+    <div className={styles.SearchField}>
+      <Autocomplete
+        freeSolo
+        id="searchField"
+        disableClearable
+        options={usersList.map((option) => option.Name)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search for User"
+            InputProps={{
+              ...params.InputProps,
+              type: "search",
+            }}
+          />
+        )}
+      />
+    </div>
+  );
+};
