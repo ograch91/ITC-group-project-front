089d4d7361003b53133375516b06f8a2c19d307e
Amnon Carmi <ace7884@hotmail.com>
2022-12-16 08:12:20 +0200

    Image Modal integrated

--- src/Components/Layout/Navbar/Navbar.jsx
+++ src/Components/Layout/Navbar/Navbar.jsx
@@ -1,35 +1,26 @@
-import { Button } from '@mui/material';
-import { useContext } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import logo from '../../../Assets/logo.png';
-import styles from './Navbar.module.css';
-import { UserAuthContext } from '../../../Context/UserAuthContext';
-import localforage from 'localforage';
-import { useState } from 'react';
-import Modal from '../../Modal/Modal';
+import { Button } from "@mui/material";
+import { useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { UserAuthContext } from "../../../Context/UserAuthContext";
+import localforage from "localforage";
+import logo from "../../../Assets/logo.png";
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
   const [auth, setAuth] = useContext(UserAuthContext);
-
-  ///move openModal usestate on profile page/pic
-  const [openModal, setOpenModal] = useState(false)
-
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    setAuth({ ...auth, user: null, isAuth: false, token: null});
-    localforage.removeItem('auth');
-    return navigate('/welcome', { replace: true });
+    setAuth({ ...auth, user: null, isAuth: false, token: null });
+    localforage.removeItem("auth");
+    return navigate("/welcome", { replace: true });
   };
 
   return (
     <div className={styles.HoverArea}>
       <div className={styles.Navbar}>
         <div className={styles.between}>
-          <img src={logo} alt="logo" onClick={() => setOpenModal(true)}/>
-
-          <Modal image={logo} open={openModal} onClose={() => setOpenModal(false)}></Modal>
-
+          <img src={logo} alt="logo" />
           <Link to="/profile">Profile</Link>
           <Link to="/home">Home</Link>
           <Button onClick={() => handleLogout()} variant="text">
@@ -40,12 +31,3 @@ export const Navbar = () => {
     </div>
   );
 };
-
-
-
-
-
-
-
-
-
