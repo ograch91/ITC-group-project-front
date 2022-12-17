4d487f6ab93bec7c3cd62e25dc77962d40db14d9
Amnon Carmi <ace7884@hotmail.com>
2022-12-17 15:35:19 +0200

    Message Submission implemented - pending auth fixup

--- src/Components/ActiveElements/SubmitMessage/SubmitMessage.jsx
+++ src/Components/ActiveElements/SubmitMessage/SubmitMessage.jsx
@@ -1,23 +1,34 @@
 import { Button } from "@mui/material";
-import React, { useState }  from "react";
+import React, { useContext, useState }  from "react";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+import { baseUrl } from "../../../Hooks/UseApi";
 import styles from "./SubmitMessage.module.css";
+import { currentChatContext } from "../../../Context/CurrentChatContext";
 
 
 export const SubmitMessage = () => {
+
+  const {currentChat} = useContext(currentChatContext);
   
   const[message,setMessage]=useState({
     id:uuidv4(),
     sender:'mosh',
-    chatid:43534,
+    chatid:currentChat,
     datesent:"",
     content:""
   });
   
-  const handleSubmit =(e)=>{
+  const handleSubmit =(e,message)=>{
+    console.log('message handle',message);
     e.preventDefault();
-
-
+    let date = new Date();
+    date = date.toLocaleString();
+    setMessage((message)=> { return {...message, datesent: date}});
+    axios.post(`${baseUrl}/messages/`,{message}).
+    catch(function (error) {
+      console.log(error);
+    });
   }
 
   return (
@@ -26,9 +37,14 @@ export const SubmitMessage = () => {
         className={styles.form}
         placeholder="What you have in mind..."
         value={message.content}
-        onChange={(e)=>setMessage((message)=>{return {...message,content:e.target.value}})}
+        onChange={(e) =>
+          setMessage((message) => {
+              return { ...message, content: e.target.value };
+            })
+            }
+       
       />
-      <Button onClick={(e)=>handleSubmit(e)}>Send</Button>
+      <Button onClick={(e)=>handleSubmit(e,message)}>Send Message</Button>
     </div>
   );
 };
